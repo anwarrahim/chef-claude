e33334cb416df7261639cea26e6d91c4eabec96f
Anwar Rahim <anwargithub@gmail.com>
2025-03-24 22:22:56 +0500

    add Header component with basic structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header(props){
+    return(
+        <>
+            <div className="container">
+                <div className="container-wrapper">
+                    <img src="" alt="" />
+                    <span></span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default Header
