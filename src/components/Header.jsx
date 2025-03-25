691a01038ef8a68644131274e49f63cb6af80846
Anwar Rahim <anwargithub@gmail.com>
2025-03-25 14:36:57 +0500

    refactor Header component styles and update body class for improved layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 function Header(props){
     return(
         <>
-            <div className="w-screen  shadow shadow-gray-400">
-                <div className="container-wrapper flex justify-center items-center my-2">
-                    <img className="w-15 h-15 mr-2" src={props.webLogo} alt="" />
-                    <span className="text-2 ">{props.webName}</span>
+            <div className="w-screen  shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] bg-white shadow-gray-400">
+                <div className="container-wrapper py-2 flex justify-center items-center">
+                    <img className="w-15 h-15 pr-2 " src={props.webLogo} alt="" />
+                    <span className="text-xl font-semibold ">{props.webName}</span>
                 </div>
             </div>
         </>
