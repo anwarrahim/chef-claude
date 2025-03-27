fb57f8e7e41a8c332c9b54e862d4fae4830086a7
Anwar Rahim <anwargithub@gmail.com>
2025-03-27 17:11:16 +0500

    refactor Main component layout and styling for improved user experience

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,22 @@
 function Main(){
     return (
         <>
-        <main className="flex  justify-center gap-1.5 mt-2">
-            <input 
-            className="max-w-[25] min-w-[10] grow border-2"
-            type="text"
-            placeholder="e.g Mirchi.. "
-            
-            />
-            <button> Add ingredients</button>
+        <main className=" mt-6">
+            <form action="" className="flex justify-center gap-1.5">
+                    <input 
+                className="max-w-lg min-w-xs grow border-2 border-gray-300 rounded-sm p-1.5"            type="text"
+                placeholder="e.g Mirchi.. "
+                
+                />
+                <button className="bg-black text-white py-1.5 px-3 border rounded-sm flex  first-letter:text-xl before:content-['+'] before:mr-1  cursor-pointer">
+                    Add ingredients
+                </button>
+            </form>
+            <ul>
+                
+            </ul>
+           
         </main>
-        
         </>
     )
 }
